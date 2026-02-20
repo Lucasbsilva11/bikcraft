298520e4a0ec4c2f67c9786236bb86d200b6e83b
Lucas Balbino <Lucas.bsilva23@hotmail.com>
2026-02-20 15:22:54 -0300

    JavaScript-galeria-bicicletas

--- js/script.js
+++ js/script.js
@@ -32,8 +32,6 @@ function ativarPergunta(event) {
   resposta.classList.toggle("ativa");
   const ativa = resposta.classList.contains("ativa");
   pergunta.setAttribute("aria-expended", ativa);
-
-  console.log(ativa)
 }
 
 function eventosPerguntas(pergunta) {
@@ -41,3 +39,22 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// galeria de bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
